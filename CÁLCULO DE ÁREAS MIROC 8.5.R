d9e7d357920a4717a8b80a8bb629ccc12287225b
rebecacz <u202010181@upc.edu.pe>
2024-09-13 18:05:32 -0500

    Update and rename AREA MIROC85.R to CÁLCULO DE ÁREAS MIROC 8.5.R

--- CÁLCULO DE ÁREAS MIROC 8.5.R
+++ CÁLCULO DE ÁREAS MIROC 8.5.R
@@ -1,4 +1,4 @@
-#ConfiguraciÃ³n inicial
+#Configuración inicial
 library(rasterVis)
 library(dichromat)
 library(raster)
@@ -8,21 +8,21 @@ library(tidyverse)
 library(raster)
 
 
-##ACCSE45LIMA###
-##########ACCES45LIMA
+##MIROC 8.5 LIMA###
+##########MIROC 8.5 LIMA
 #Cargar datos
 map <- raster("D:/Maxent/AREA Y LIMA2/Vasconcellea_candicans_MIROC85.asc")
 plot(map)
 
-#_______AREA KM2 
+#_______ÁREA KM2 
 ####################
 #library(raster)
 #library(rgdal)
 #summary(map)
 ####Mapa de raster estilo lucia
 
-###reproyectar
-#### l,imite de lima
+###Reproyectar
+#### Límite de Lima
 mps <- shapefile("D:/Maxent/AREA Y LIMA2/LIMA.shp")
 plot(mps)
 #hillshade_proj <- projectRaster(hillshade, crs = crs(b5))
@@ -50,8 +50,8 @@ levelplot(cat.act, at = breaks, col.regions = cols, main = "       MIROC6 RCP 8.
 
 #------------------------------------
 #------------------------------------
-#####AREA ACTUAL
-###resolucion en grados geograficos 
+#####ÁREA ACTUAL
+###Resolución en grados geográficos 
 
 res(cat.act) #0.008333° y un grados equivale a 111.32km
 area <- res(cat.act)[1]*res(cat.act)[2]*111.32*111.32 #en Km cuadrados
@@ -87,7 +87,7 @@ plot(mps, add=TRUE, border='dark grey')
 #        margin=T) + spplot(mps, fill = "transparent", col = "white")
 levelplot(lima, at = breaks, col.regions = cols, main = "       MIROC6 RCP 8.5 (2081 - 2100)",
           margin=F) + spplot(mps, fill = "transparent", col = "white")
-###resolucion en grados geograficos 
+###Resolución en grados geográficos 
 res(lima) #0.008333° y un grados equivale a 111.32km
 area1 <- res(lima)[1]*res(lima)[2]*111.32*111.32 #en Km cuadrados
 area1
